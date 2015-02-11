448fb167def0121e6d0cf499ca4a1700e901216f
Xavier Gonzalez <xavier@gonzalez.pm>
2015-02-11 16:36:24 +0100

    correction authent

--- admin/classes/Authentication.php
+++ admin/classes/Authentication.php
@@ -7,14 +7,13 @@ class Authentication extends StorageManager {
 	}
 	
 	public function grantAccess($login,$mdp){
-		 $this->dbConnect();
+		$this->dbConnect();
 		$requete = "SELECT * FROM admin";
-		$requete .= " WHERE login = '" . mysql_real_escape_string( $login ) . "'";
-		$requete .= " AND mdp = '" . mysql_real_escape_string( $mdp ) . "'";
-		//echo $requete . "<br><br>";
+		$requete .= " WHERE login = '" . mysqli_real_escape_string($this->mysqli, $login ) . "'";
+		$requete .= " AND mdp = '" . mysqli_real_escape_string($this->mysqli, $mdp ) . "'";
+		//echo $requete . "<br><br>";exit();
 		$result = mysqli_query($this->mysqli,$requete);
-		$num_rows = mysql_num_rows($result);
-	
+		$num_rows = mysqli_num_rows($result);
 		$this->dbDisConnect();
 		if ($num_rows > 0)  {
 			return true;
